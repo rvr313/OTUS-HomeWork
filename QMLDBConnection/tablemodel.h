44368b9d1871aa18ce6bcd6d982e997a08ce4786
rvr313 <rybalkin.roman@gmail.com>
2025-08-18 19:57:46 +0300

    Update tablemodel.h

--- QMLDBConnection/tablemodel.h
+++ QMLDBConnection/tablemodel.h
@@ -23,13 +23,11 @@ public:
     // QAbstractItemModel interface
     QVariant data(const QModelIndex &index, int role) const override;
     QHash<int, QByteArray> roleNames() const override;
-    //QVariant headerData(int section, Qt::Orientation orientation, int role) const override;
 
 public slots:
     void allDataChanged();
 
 private:
-    QStringList m_headers = {tr("Date Time"), tr("Purchase Price"), tr("Sell Price")};
     QStringList m_roles = {"datetime", "purchase", "sell", "changing"};
 };
 
