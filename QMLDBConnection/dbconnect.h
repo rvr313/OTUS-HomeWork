c2118a07455aacd903e52627daf2b329dd070d2b
rvr313 <rybalkin.roman@gmail.com>
2025-08-18 19:52:39 +0300

    Update dbconnect.h

--- QMLDBConnection/dbconnect.h
+++ QMLDBConnection/dbconnect.h
@@ -7,17 +7,25 @@
 class DbConnect : public QObject
 {
     Q_OBJECT
+    Q_PROPERTY(bool connected READ connected WRITE setConnected NOTIFY dbConnected)
 public:
     explicit DbConnect(QObject *parent = nullptr);
+    bool connected() const;
 
-public slots:
-    Q_INVOKABLE void connectDb();
+    static bool addDatabase();
 
 signals:
     void dbConnected();
 
+public slots:
+    void connectDb();
+
+protected slots:
+    void setConnected(bool connected);
+
 private:
     QSqlDatabase *db{nullptr};
+    bool m_connected{false};
 };
 
 #endif // DBCONNECT_H
